c289c366780a9851782062eaa7667eb7e439f172
Roelf Wichertjes <contact@roelf.org>
2020-03-17 00:02:46 +0100

    Refactor gOwO into library.

--- cmd/gowo/main.go
+++ cmd/gowo/main.go
@@ -4,68 +4,10 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"math/rand"
 	"os"
-	"strings"
+
+	gowo "github.com/rewbycraft/gOwO/pkg/gowo"
 )
-
-var prefixes = []string{
-	"<3 ",
-	"0w0",
-	"H-hewwo?? ",
-	"HIIII! ",
-	"Haiiii! ",
-	"Huohhhh. ",
-	"OWO ",
-	"OwO ",
-	"UwU ",
-}
-
-var suffixes = []string{
-	" :3",
-	" UwU",
-	" ʕʘ‿ʘʔ",
-	" >_>",
-	" ^_^",
-	"..",
-	" Huoh.",
-	" ^-^",
-	" ;_;",
-	" ;-;",
-	" xD",
-	" x3",
-	" :D",
-	" :P",
-	" ;3",
-	" XDDD",
-	", fwendo",
-	" ㅇㅅㅇ",
-	" (人◕ω◕)",
-	"（＾ｖ＾）",
-	" Sigh.",
-	" x3",
-	" ._.",
-	" (• o •)",
-	" >_<",
-}
-
-var subs = map[string]string{
-	"r": "w",
-	"l": "w",
-	"R": "W",
-	"L": "W",
-	//  "ow": "OwO",
-	"no":   "nu",
-	"has":  "haz",
-	"have": "haz",
-	"you":  "uu",
-	"the ": "da ",
-	"The ": "Da ",
-}
-
-func getRandomFromSlice(slice []string) string {
-	return slice[rand.Intn(len(slice))]
-}
 
 func main() {
 	prefixPtr := flag.Bool("prefix", true, "Add a prefix")
@@ -79,22 +21,7 @@ func main() {
 		if err != nil {
 			break
 		}
-		prefix := ""
-		suffix := ""
-		if *prefixPtr {
-			prefix = getRandomFromSlice(prefixes)
-		}
 
-		if *suffixPtr {
-			suffix = getRandomFromSlice(suffixes)
-		}
-
-		replacedString := string(text)
-
-		for k, v := range subs {
-			replacedString = strings.Replace(replacedString, k, v, -1)
-		}
-
-		fmt.Printf("%s%s%s\n", prefix, replacedString, suffix)
+		fmt.Println(gowo.OwOify(string(text), *prefixPtr, *suffixPtr))
 	}
 }
---
+++ pkg/gowo/gowo.go
@@ -0,0 +1,85 @@
+package gowo
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+var prefixes = []string{
+	"<3 ",
+	"0w0",
+	"H-hewwo?? ",
+	"HIIII! ",
+	"Haiiii! ",
+	"Huohhhh. ",
+	"OWO ",
+	"OwO ",
+	"UwU ",
+}
+
+var suffixes = []string{
+	" :3",
+	" UwU",
+	" ʕʘ‿ʘʔ",
+	" >_>",
+	" ^_^",
+	"..",
+	" Huoh.",
+	" ^-^",
+	" ;_;",
+	" ;-;",
+	" xD",
+	" x3",
+	" :D",
+	" :P",
+	" ;3",
+	" XDDD",
+	", fwendo",
+	" ㅇㅅㅇ",
+	" (人◕ω◕)",
+	"（＾ｖ＾）",
+	" Sigh.",
+	" x3",
+	" ._.",
+	" (• o •)",
+	" >_<",
+}
+
+var subs = map[string]string{
+	"r": "w",
+	"l": "w",
+	"R": "W",
+	"L": "W",
+	//  "ow": "OwO",
+	"no":   "nu",
+	"has":  "haz",
+	"have": "haz",
+	"you":  "uu",
+	"the ": "da ",
+	"The ": "Da ",
+}
+
+func getRandomFromSlice(slice []string) string {
+	return slice[rand.Intn(len(slice))]
+}
+
+func OwOify(text string, enablePrefix bool, enableSuffix bool) string {
+	prefix := ""
+	suffix := ""
+	if enablePrefix {
+		prefix = getRandomFromSlice(prefixes)
+	}
+
+	if enableSuffix {
+		suffix = getRandomFromSlice(suffixes)
+	}
+
+	replacedString := string(text)
+
+	for k, v := range subs {
+		replacedString = strings.Replace(replacedString, k, v, -1)
+	}
+
+	return fmt.Sprintf("%s%s%s", prefix, replacedString, suffix)
+}
